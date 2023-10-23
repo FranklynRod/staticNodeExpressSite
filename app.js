76743add907984310161d30bdd626f1fb9199ba2
Franklyn Rodriguez <franklynrodriguez.swe@gmail.com>
2023-10-23 16:56:23 -0400

    Added to pug template

--- app.js
+++ app.js
@@ -2,7 +2,22 @@ const express = require('express');
 
 const app = express();
 
+const colors = [
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'purple'
+];
+
 app.set('view engine', 'pug');
+
+app.get('/'(req, res) => {
+    res.render('index');
+});
+
+app.get()
 
 
 app.listen(3000)
