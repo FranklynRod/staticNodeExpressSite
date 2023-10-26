0eccafc4b22b5ab4c720760e3679a7e19cd130ef
Franklyn Rodriguez <franklynrodriguez.swe@gmail.com>
2023-10-26 11:53:56 -0400

    Added routes

--- app.js
+++ app.js
@@ -1,23 +1,35 @@
 const express = require('express');
+const data = require('./data.json');
+const {projects} = data;
+const bodyParser = require('body-parser');
 
 const app = express();
 
-const colors = [
-    'red',
-    'orange',
-    'yellow',
-    'green',
-    'blue',
-    'purple'
-];
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('static', express.static('public')),
 
 app.set('view engine', 'pug');
 
-app.get('/'(req, res) => {
-    res.render('index');
+
+// GET Homepage
+app.get('/',(req,res) =>{
+    res.render('index',  {projects} );
+})
+
+//GET about
+app.get('/about',(req,res) =>{
+    res.render('about');
+})
+
+// GET Projects
+app.get('/projects/:id',(req,res) =>{
+    const projectId = req.params.id
+    const foundId = projects.find(({id}) => id === parseInt(projectId))
+    res.render('project',{foundId});
+})
+
+// Errors
+
+app.listen(3000, () => {
+    console.log('Serves Port 3000')
 });
-
-app.get()
-
-
-app.listen(3000)
